8252ab7ce886aee4e3e0b44be6cb4cda2c30daf1
Gurpreet Singh <gurpreet@mailfence.com>
2018-04-08 18:50:52 -0500

    Fix paths and begin running compiled research project

--- research/BugLocalization/src/main/java/gr/ntua/softlab/filepaths/Paths.java
+++ research/BugLocalization/src/main/java/gr/ntua/softlab/filepaths/Paths.java
@@ -3,30 +3,20 @@ package gr.ntua.softlab.filepaths;
 //TODO: Destroy hardcoded file paths
 public class Paths
 {
-    public static final String RSF_FOLDER = "../../Data_FileSystem/RSFS/";
-    public static final String ACDC_INPUT_PATH = "../../Data_FileSystem/ACDC_INPUT/";
-    public static final String ACDC_OUTPUT_PATH = "../../Data_FileSystem/CLUSTERED_SYSTEMS/";
-    public static final String ACDC_FILE_INPUT_PATH = "../../Data_FileSystem/ACDC_FILE_INPUT/";
-    public static final String ACDC_FILE_OUTPUT_PATH = "../../Data_FileSystem/FILE_CLUSTERED_SYSTEMS/";
-    public static final String CDIF_FOLDER = "../../Data_FileSystem/CDIFS/";
-    public static final String BRLists = "../../Data_FileSystem/BRLists/";
-    public static final String FINALINPUTBRS = "../../Data_FileSystem/Inputs/"; // _LSA
-    public static final String GOLDSTANDARD = "../../Data_FileSystem/GoldStandardPerProject/"; // GS
-    public static final String QUERYBRS = "../../Data_FileSystem/FinalInputBRs/"; // _noLSA
-    public static final String SOLUTION_ACDC_INPUT = "../../Data_FileSystem/SOLUTION_ACDC_INPUT/";
-    public static final String SOLUTION_CLUSTERS = "../../Data_FileSystem/SOLUTION_CLUSTERS/";
-    public static final String FIRSTLEVELREPORTS = "../../Data_FileSystem/Reports/First_Reports/";
-    public static final String ZEROREPORTS = "../../Data_FileSystem/Reports/Zero_Reports/";
-    public static final String BIGREPORTS = "../../Data_FileSystem/Reports/Big_Reports/";
-    public static final String URANKEDREPORTS = "../../Data_FileSystem/Reports/U_Ranked_Reports/";
-
-    //UNUSED
-    public static String AUXILIARY_RSF_FOLDER = "../../Data_FileSystem/Auxiliary_RSFS/";
-    public static String OUTPUT_FOLDER = "../../Data_FileSystem/File_RSFS/";
-    public static String ACDC_PATH = "../../Data_FileSystem/CLUSTERED_SYSTEMS/";
-    public static String ACDC_FILE_PATH = "../../Data_FileSystem/FILE_CLUSTERED_SYSTEMS/";
-    public static String BUNCH_FILE_PATH = "../../Data_FileSystem/BUNCHOUTPUT/";
-    public static String LIN_DEREK_CLUSTERS_PATH = "../../Data_FileSystem/LIN_DEREK_GROUND_TRUTHS/";
-    public static String GENERIC_OUTPUT_FOLDER = "../../Data_FileSystem/GENERIC_OUTPUT_FOLDER/";
-    public static String PRODUCT_SOURCE_CODE = "../../Data_FileSystem/PRODUCT_SOURCE_CODE/";
+    public static final String RSF_FOLDER = "../Data_FileSystem/RSFS/";
+    public static final String ACDC_INPUT_PATH = "../Data_FileSystem/ACDC_INPUT/";
+    public static final String ACDC_OUTPUT_PATH = "../Data_FileSystem/CLUSTERED_SYSTEMS/";
+    public static final String ACDC_FILE_INPUT_PATH = "../Data_FileSystem/ACDC_FILE_INPUT/";
+    public static final String ACDC_FILE_OUTPUT_PATH = "../Data_FileSystem/FILE_CLUSTERED_SYSTEMS/";
+    public static final String CDIF_FOLDER = "../Data_FileSystem/CDIFS/";
+    public static final String BRLists = "../Data_FileSystem/BRLists/";
+    public static final String FINALINPUTBRS = "../Data_FileSystem/Inputs/"; // _LSA
+    public static final String GOLDSTANDARD = "../Data_FileSystem/GoldStandardPerProject/"; // GS
+    public static final String QUERYBRS = "../Data_FileSystem/FinalInputBRs/"; // _noLSA
+    public static final String SOLUTION_ACDC_INPUT = "../Data_FileSystem/SOLUTION_ACDC_INPUT/";
+    public static final String SOLUTION_CLUSTERS = "../Data_FileSystem/SOLUTION_CLUSTERS/";
+    public static final String FIRSTLEVELREPORTS = "../Data_FileSystem/Reports/First_Reports/";
+    public static final String ZEROREPORTS = "../Data_FileSystem/Reports/Zero_Reports/";
+    public static final String BIGREPORTS = "../Data_FileSystem/Reports/Big_Reports/";
+    public static final String URANKEDREPORTS = "../Data_FileSystem/Reports/U_Ranked_Reports/";
 }
